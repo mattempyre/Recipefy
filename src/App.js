60ebc5598054a6880bf600b2543435538431e2b4
Matthew Fogarty <mattempyre@gmail.com>
2023-07-31 14:12:44 +0200

    Finished Hero Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './pages/Home';
+
+import RecipeDetail from './pages/RecipeDetail';
+
+function Layout() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <h1>This is a test</h1>
+    <div className="bg-black">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="recipes/:id" element={<RecipeDetail />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
