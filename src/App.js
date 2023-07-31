ffd27986d740c9f2687fb248a25fbeb8d3801fcb
Matthew Fogarty <mattempyre@gmail.com>
2023-07-31 14:16:23 +0200

    Add Navbar and Footer to Layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
 
 import RecipeDetail from './pages/RecipeDetail';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function Layout() {
   return (
     <>
+      <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 }
